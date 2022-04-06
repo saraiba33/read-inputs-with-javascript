2eb428baa19bebd55e37a35eafa6295e05afcae4
Sara Ibarra <saraiba33@gmail.com>
2022-04-06 17:49:07 -0600

    add solution

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-function logValues(){
-  // Your code here
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const email = document.querySelector("#email");
+
+
+function logValues() {
+    console.log(firstName.value, lastName.value, email.value)
 }
